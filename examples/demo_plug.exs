bfd930a37a7c0e02a60e32140204b180f2baf9ee
Wojtek Mach <wojtek@wojtekmach.pl>
2024-04-29 15:00:45 +0200

    Update examples/demo_plug.exs

--- examples/demo_plug.exs
+++ examples/demo_plug.exs
@@ -1,12 +1,10 @@
 #!/usr/bin/env elixir
 Mix.install([
-  {:phoenix_playground, "~> 0.1.0"}
+  {:phoenix_playground, "~> 0.1.2"}
 ])
 
 PhoenixPlayground.start(
   plug: fn conn ->
-    conn
-    |> Plug.Conn.put_resp_content_type("text/html")
-    |> Plug.Conn.send_resp(200, "Hello!")
+    Plug.Conn.send_resp(conn, 200, "Hello!")
   end
 )
