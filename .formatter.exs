6a7898a19e30daad61fb0863ae3dd887f60a82d1
Mario Uher <uher.mario@gmail.com>
2024-07-14 19:02:04 +0200

    Ensure examples are also formatted

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,5 @@
 # Used by "mix format"
 [
   import_deps: [:phoenix],
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,examples,lib,test}/**/*.{ex,exs}"]
 ]
